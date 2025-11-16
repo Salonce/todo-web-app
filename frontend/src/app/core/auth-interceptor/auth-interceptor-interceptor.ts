2eafe59613065442d7ccffd27d43b1d90ca6e782
saloncey <miedzyniebem@gmail.com>
2025-11-17 00:00:40 +0100

    removed resending intercepted request

--- frontend/src/app/core/auth-interceptor/auth-interceptor-interceptor.ts
+++ frontend/src/app/core/auth-interceptor/auth-interceptor-interceptor.ts
@@ -1,15 +1,15 @@
-import { HttpEvent, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { HttpEvent, HttpInterceptorFn, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable, catchError, of, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next) => {
   return next(req).pipe(
     catchError(err => {
 
       if (req.url.endsWith('/auth') || req.url.endsWith('/logout')) {
-        return next(req); 
+        return of(new HttpResponse({ body: null }));
       }
 
-      if (err.status === 401 || err.status === 403) {
+      if (err.status === 401) {
         window.location.href = 'http://localhost:8080/oauth2/authorization/google';
       }
       return throwError(() => err);

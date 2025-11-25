92c9de7aff8388963386850413bcbff6d4c7f8de
saloncey <miedzyniebem@gmail.com>
2025-11-25 01:22:24 +0100

    added manage articles page but it is not proper yet

--- frontend/src/app/pages/article-edit-list/article-edit-list.ts
+++ frontend/src/app/pages/article-edit-list/article-edit-list.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../../core/models/article';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-edit-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-edit-list.html',
   styleUrl: './article-edit-list.css'
 })
 export class ArticleEditList {
+  constructor(private sanitizer: DomSanitizer){}
 
+  @Input() articles : Article[] = [];
+
+  getPreview(content: string, sentences: number = 3): string {
+    // naive example: split by sentences
+    const match = content.match(/.*?[.!?](\s|$)/g);
+    if (!match) return content;
+    const preview = match.slice(0, sentences).join(' ');
+    return preview; // keep HTML tags intact
+  }
+
+  getSafeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
 }

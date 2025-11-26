c9d17976428392f5f85f23f739b8bf69c18701b5
saloncey <miedzyniebem@gmail.com>
2025-11-26 19:18:02 +0100

    editing works

--- backend/todolist/src/main/java/salonce/dev/todolist/article/presentation/ArticleMapper.java
+++ backend/todolist/src/main/java/salonce/dev/todolist/article/presentation/ArticleMapper.java
@@ -5,6 +5,6 @@ import salonce.dev.todolist.article.presentation.dtos.ArticleViewResponse;
 
 public class ArticleMapper {
     public static ArticleViewResponse toArticleResponse(Article article){
-        return new ArticleViewResponse(article.getTitle(), article.getSlug(), article.getContent(), article.getAuthor().getName(), article.getCreatedAt(), article.getUpdatedAt());
+        return new ArticleViewResponse(article.getId(), article.getTitle(), article.getSlug(), article.getContent(), article.getAuthor().getName(), article.getCreatedAt(), article.getUpdatedAt());
     }
 }
--- backend/todolist/src/main/java/salonce/dev/todolist/article/presentation/dtos/ArticleViewResponse.java
+++ backend/todolist/src/main/java/salonce/dev/todolist/article/presentation/dtos/ArticleViewResponse.java
@@ -2,5 +2,5 @@ package salonce.dev.todolist.article.presentation.dtos;
 
 import java.time.LocalDateTime;
 
-public record ArticleViewResponse(String title, String slug, String content, String authorName, LocalDateTime createdAt, LocalDateTime updatedAt){
+public record ArticleViewResponse(Long id, String title, String slug, String content, String authorName, LocalDateTime createdAt, LocalDateTime updatedAt){
 }
